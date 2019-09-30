658af0bb5b73857fc35312fafdd3524348da0e0d
Israel Rojas <israelrojasshema@gmail.com>
2019-09-30 13:23:28 -0500

    Updates

---
+++ assets/logic.js
@@ -0,0 +1,9 @@
+$("#about").click(function() {
+  window.scrollTo(0, 0);
+});
+$("#portfolio").click(function() {
+  window.scrollTo(0, 680);
+});
+$("#contacts").click(function() {
+  window.scrollTo(0, 2000);
+});
